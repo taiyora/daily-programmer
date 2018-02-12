47c28145dcff130991a9032c3401f1bf7fb2ad23
taiyora <taiyora@protonmail.com>
2018-02-12 23:06:42 +1300

    [#350 Hard] Solved

---
+++ Solutions/#350 Hard/350h.py
@@ -0,0 +1,66 @@
+# Using a bloom filter and a bitarray, detect repeat values with variable probability of correctness.
+
+# This approach works by taking a number, hashing it X amount of times, converting those hashes to
+# max-bounded indices, then setting those indices of a bitarray to True.
+# To check if a number has been "seen", just do the same thing but check if all the indices are True.
+# If they are, then the number has *probably* been seen.
+# False positives are possible, and become more likely the smaller the bitarray is and the more
+# values are processed.
+
+# This implementation doesn't remember historical values between runs, as the problem description
+# seems to say is a requirement. It'd be easy to add by saving the bitarray to file, but is that what
+# is meant?
+
+import Isaac
+import mmh3
+import sys
+from bitarray import bitarray
+
+# PRNG
+isaac = Isaac.Isaac()
+
+# Bloom Filter
+bfilter_size = 2 ** 25 # ~4.2 Mb
+bloom_filter = bitarray(bfilter_size)
+
+# Hashing
+do_n_hashes = 5
+
+# Functions
+def get_indices(value):
+	'''Returns the indices that specify where to store or find the specified value.'''
+
+	indices = []
+
+	for seed in range(0, do_n_hashes):
+		index = mmh3.hash(str(value), seed) % bfilter_size
+		indices.append(index)
+
+	return indices
+
+def store(value):
+	for index in get_indices(value):
+		bloom_filter[index] = True
+
+def get(value):
+	'''Returns False if the value does not exist (for sure), or True if it exists (probably).'''
+
+	for index in get_indices(value):
+		if not bloom_filter[index]:
+			return False
+
+	return True
+
+# Run until a unique value is observed twice
+if __name__ == '__main__':
+	count = 0
+
+	while True:
+		value = isaac.rand(sys.maxsize)
+		count += 1
+		
+		if get(value):
+			print(value, 'was observed twice, after', count, 'runs')
+			break
+
+		store(value)
---
+++ Solutions/#350 Hard/Isaac.py
@@ -0,0 +1,235 @@
+"""
+        Isaac CSPRNG
+        Usage:
+            import Isaac()
+            x = Isaac.Isaac(noblock=False)
+            y = x.rand(42) # 0<= y <= 41
+        Warning: If you don't have /dev/random(noblock=False) nor /dev/urandom(noblock=True) Pythons builtin Random() will be used
+"""
+import random
+import os
+from math import ceil
+from hashlib import sha512
+
+try:
+    import psyco
+    psyco.full()
+except:
+    pass
+
+class Isaac(object):
+    def __init__(self, noblock = False, miniseed = True):
+        self.mm = [0]*256
+        self.randrsl = [0]*256
+        self.randcnt = None
+        self.aa = 0
+        self.bb = 0
+        self.cc = 0
+        random.seed()
+
+        # If noblock is set to "True" on init it will seed from /dev/urandom
+        rnd_source = '/dev/urandom' if noblock else '/dev/random'
+        if os.path.exists(rnd_source):
+            f = open(rnd_source, 'r')
+            if miniseed:
+                # Reads 16 bytes = 128 bits and expands it to 8192 bits with hashing
+                z = f.read(16)
+                digests = ""
+                for i in range(2048/128): # ensures we run the hash enough times to get 8192 bits
+                    z = sha512(z).hexdigest()
+                    digests += z
+                out = ""
+                marker = 0
+                for i in range(0,len(digests), 8):
+                    y = int(bin(int(digests[i:i+8], base=16))[2:].rjust(32, "0"), base=2)
+                    self.randrsl[marker] = y
+                    marker += 1
+                    
+            else:
+                # Reads 4 bytes for every integer. => 4*256= 1024 bytes=8192 bits
+                for x in range(256):
+                    z = f.read(4)
+                    # String to binary and back to larger integer
+                    y = int("".join([bin(ord(i))[2:].rjust(8,"0") for i in list(z)]), base=2)
+                    self.randrsl[x] = y
+            f.close()
+        else:
+            for x in range(256):
+                self.randrsl[x] = random.randint(1,4294967294)
+            
+        self.__randinit__(True)
+
+    def rand(self, num):
+        if self.randcnt == 1:
+            self.__isaac__()
+            self.randcnt = 256
+
+        self.randcnt -= 1
+
+        return self.randrsl[self.randcnt]%num
+
+    def bits(self, num):
+        count = ceil(num/32.0)
+        bitlist = ""
+        
+        for x in range(count):
+            bitlist += (bin(self.rand(4294967294))[2:].rjust(32, "0"))
+        return bitlist[:num]
+
+    def __isaac__(self):
+        x = 0
+        y = 0
+        i = 0
+        
+        self.cc += 1
+        self.bb += self.cc
+        self.bb &= 0xffffffff
+
+    
+        while i < 256:
+            x = self.mm[i]
+            self.aa = (self.mm[(i + 128) & 255] + (self.aa^(self.aa << 13)) ) & 0xffffffff
+            self.mm[i] = y = (self.mm[(x>>2)&255] + self.aa + self.bb ) & 0xffffffff
+            self.randrsl[i] = self.bb = (self.mm[(y>>10)&255] + x ) & 0xffffffff
+            i += 1
+     
+            x = self.mm[i]
+            self.aa = (self.mm[(i+128)&255] + (self.aa^(0x03ffffff & (self.aa >> 6))) ) & 0xffffffff
+            self.mm[i] = y = (self.mm[(x>>2)&255] + self.aa + self.bb ) & 0xffffffff
+            self.randrsl[i] = self.bb = (self.mm[(y>>10)&255] + x ) & 0xffffffff
+            i += 1
+     
+            x = self.mm[i]
+            self.aa = (self.mm[(i + 128)&255] + (self.aa^(self.aa << 2)) ) & 0xffffffff
+            self.mm[i] = y = (self.mm[(x>>2)&255] + self.aa + self.bb ) & 0xffffffff
+            self.randrsl[i] = self.bb = (self.mm[(y>>10)&255] + x ) & 0xffffffff
+            i += 1
+     
+            x = self.mm[i]
+            self.aa = (self.mm[(i+128)&255] + (self.aa^(0x0000ffff & (self.aa >> 16))) ) & 0xffffffff
+            self.mm[i] = y = (self.mm[(x>>2)&255] + self.aa + self.bb ) & 0xffffffff
+            self.randrsl[i] = self.bb = (self.mm[(y>>10)&255] + x ) & 0xffffffff
+            i += 1
+
+    def __randinit__(self, flag):
+        a=b=c=d=e=f=g=h = int("9e3779b9", base=16)
+        self.aa = self.bb = self.cc = 0
+
+        for x in range(4):
+            a ^= b<<1
+            d += a
+            b += c
+            b ^= 0x3fffffff & (c>>2)
+            e += b
+            c += d
+            c ^= d << 8
+            f += c
+            d += e
+            d ^= 0x0000ffff & (e >> 16)
+            g += d
+            e += f
+            e ^= f << 10
+            h += e
+            f += g
+            f ^= 0x0fffffff & (g >> 4)
+            a += f
+            g += h
+            g ^= h << 8
+            b += g
+            h += a
+            h ^= 0x007fffff & (a >> 9)
+            c += h
+            a += b
+
+        i = 0
+        while i < 256:
+            if flag:
+                a+=int(self.randrsl[i])
+                b+=int(self.randrsl[i+1])
+                c+=self.randrsl[i+2]
+                d+=self.randrsl[i+3]
+                e+=self.randrsl[i+4]
+                f+=self.randrsl[i+5]
+                g+=self.randrsl[i+6]
+                h+=self.randrsl[i+7]
+
+            a^=b<<11
+            d+=a
+            b+=c
+            b^=0x3fffffff & (c>>2)
+            e+=b
+            c+=d
+            c^=d<<8
+            f+=c
+            d+=e
+            d^=0x0000ffff & (e>>16)
+            g+=d
+            e+=f
+            e^=f<<10
+            h+=e
+            f+=g
+            f^=0x0fffffff & (g>>4)
+            a+=f
+            g+=h
+            g^=h<<8
+            b+=g
+            h+=a
+            h^=0x007fffff & (a>>9)
+            c+=h
+            a+=b
+            self.mm[i]=a
+            self.mm[i+1]=b
+            self.mm[i+2]=c
+            self.mm[i+3]=d
+            self.mm[i+4]=e
+            self.mm[i+5]=f
+            self.mm[i+6]=g
+            self.mm[i+7]=h
+            i += 8
+
+        if flag:
+            i = 0
+            while i < 256:
+                a+=self.mm[i]
+                b+=self.mm[i+1]
+                c+=self.mm[i+2]
+                d+=self.mm[i+3]
+                e+=self.mm[i+4]
+                f+=self.mm[i+5]
+                g+=self.mm[i+6]
+                h+=self.mm[i+7]
+                a^=b<<11
+                d+=a
+                b+=c
+                b^=0x3fffffff & (c>>2)
+                e+=b
+                c+=d
+                c^=d<<8
+                f+=c
+                d+=e
+                d^=0x0000ffff & (e>>16)
+                g+=d
+                e+=f
+                e^=f<<10
+                h+=e
+                f+=g
+                f^=0x0fffffff & (g>>4)
+                a+=f
+                g+=h
+                g^=h<<8
+                b+=g
+                h+=a
+                h^=0x007fffff & (a>>9)
+                c+=h
+                a+=b
+                self.mm[i]=a
+                self.mm[i+1]=b
+                self.mm[i+2]=c
+                self.mm[i+3]=d
+                self.mm[i+4]=e
+                self.mm[i+5]=f
+                self.mm[i+6]=g
+                self.mm[i+7]=h
+                i += 8
+        self.__isaac__()
+        self.randcnt=256
